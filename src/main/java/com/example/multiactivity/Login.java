43bd8aae7d6dc7a9fc0e22284f29f0870b771ebe
vibhorjain338 <vibhor.jain338@gmail.com>
2021-03-02 19:11:25 +0530

    initial commit

--- src/main/java/com/example/multiactivity/Login.java
+++ src/main/java/com/example/multiactivity/Login.java
@@ -3,24 +3,52 @@ package com.example.multiactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-    Button login;
+    Button loginBtn;
+    EditText loginUserName, loginPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        login = (Button)findViewById(R.id.button3);
-        login.setOnClickListener(new View.OnClickListener() {
+
+        loginBtn = (Button)findViewById(R.id.button3);
+        loginUserName = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        loginPassword = (EditText) findViewById(R.id.editTextTextPassword);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent login = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(login);
-                Toast.makeText(getApplicationContext(),"Login Successful !!!",Toast.LENGTH_LONG).show();
+                String loginUser = loginUserName.getText().toString();
+                String loginPass = loginPassword.getText().toString();
+                SharedPreferences sharedPref = getSharedPreferences("userData", MODE_PRIVATE);
+                String regUser = sharedPref.getString("regUser","");
+                String regPass = sharedPref.getString("regPass","");
+                if(!TextUtils.isEmpty(loginUser)||!TextUtils.isEmpty(loginPass)){
+                    if(loginUser.equals(regUser)&&loginPass.equals(regPass)){
+                        Toast.makeText(getApplicationContext(),"Welcome To RareProb !!!",Toast.LENGTH_LONG).show();
+                        Intent login = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(login);
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(),"Enter a valid Username or Password...",Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"!!!Username or Password cann't be Blank!!!\n Try Again...",Toast.LENGTH_LONG).show();
+                    
+                }
+
+
             }
         });
     }
--- src/main/java/com/example/multiactivity/Register.java
+++ src/main/java/com/example/multiactivity/Register.java
@@ -3,22 +3,45 @@ package com.example.multiactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
-    private Button signup;
+    EditText regUserName, regPassword;
+    private Button signupBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        signup = (Button)findViewById(R.id.button6);
-        signup.setOnClickListener(new View.OnClickListener() {
+        signupBtn = (Button)findViewById(R.id.button6);
+        regUserName = (EditText) findViewById(R.id.editTextTextEmailAddress2);
+        regPassword = (EditText) findViewById(R.id.editTextTextPassword2);
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
+                String regUser = regUserName.getText().toString();
+                String regPass = regPassword.getText().toString();
+                if(TextUtils.isEmpty(regUser)||TextUtils.isEmpty(regPass)){
+                    Toast.makeText(getApplicationContext(),"UserName or Password Cant be blank...",Toast.LENGTH_LONG).show();
+
+                }
+                else{
+                    SharedPreferences sharedPref = getSharedPreferences("userData",MODE_PRIVATE);
+                    SharedPreferences.Editor edit = sharedPref.edit();
+
+                    edit.putString("regUser", regUser);
+                    edit.putString("regPass",regPass);
+                    edit.commit();
+                    Toast.makeText(getApplicationContext(),"Registration Successful \n Login Yourself :)",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
